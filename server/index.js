934ee2dfb5b3fc17fe499933d8fdf6c9936c0789
Bradley Lund <bradjlund@gmail.com>
2020-06-14 12:59:41 +0200

    make server with express

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(function(req,res,next) {
+    res.header("Access-Control-Allow-Origin","*"); //disabled for security on local
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next()
+})
+
+app.get('/creators', async (req, res) => {
+    const creators = [
+        {name: 'Code Drip', img: 'https://'},
+        {name: 'Dave Lee', img: 'https://'},
+        {name: 'MKBHD', img: 'https://'},
+    ];
+    //todo: GET from DataBase
+    res.send(creators)
+})
+
+app.post('/creators', async (req, res) => {
+    console.log(req.body)
+    // todo: Scrape channel 
+    // todo: Add to database
+    res.send('success')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
